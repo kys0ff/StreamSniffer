f51349a2dfd48cc9b5f5ad63b0c40a8b2511771a
kys0ff <idk683120@gmail.com>
2026-04-20 09:32:38 +0100

    Initial commit

---
+++ app/src/main/java/off/kys/sniffer/MainActivity.kt
@@ -0,0 +1,28 @@
+package off.kys.sniffer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import off.kys.sniffer.ui.theme.StreamSnifferTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            StreamSnifferTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
