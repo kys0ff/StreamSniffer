db97bf3b54696636776f6d8c297b1bf565795e24
kys0ff <idk683120@gmail.com>
2026-04-20 12:52:14 +0100

    Update versionName to 0.1.0 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         //noinspection OldTargetApi
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
